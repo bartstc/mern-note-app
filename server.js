6453f79316646d1355f4f01466f20df60b675f68
Bart <bartst010395@gmail.com>
2019-02-18 14:28:57 +0100

    Prepared to deploy

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ mongoose.connect(db, { useNewUrlParser: true })
 app.use('/notes', notes);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build')); // run index.html in build folder
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build')); // run index.html in build folder
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-};
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// };
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
